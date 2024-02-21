9cc1e798d178eea871aa9d0ff7b93d1eb8cfba13
Caio Abdon <a.caio@cicclo.com>
2024-02-21 00:39:16 -0300

    refactory: create app.ts and move config express, initialize db connection in server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,30 +1,9 @@
 import "reflect-metadata";
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import AppError from "@shared/errors/AppError";
-import routes from "./routes";
+import dataSource from "@shared/typeorm";
+import { app } from "./app";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use(routes);
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  },
-);
-
-app.listen(3000, () => {
-  console.log("O Servidor está rodando na porta 3000");
+dataSource.initialize().then(() => {
+  app.listen(3000, () => {
+    console.log("O servidor está rodando na porta 3000!");
+  });
 });
