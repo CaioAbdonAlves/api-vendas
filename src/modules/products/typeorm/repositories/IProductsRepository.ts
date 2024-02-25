28074be35d1127e18fe970c13dc39cf622171e3b
Caio Abdon <a.caio@cicclo.com>
2024-02-25 02:41:23 -0300

    feat: Create a interface for ProductRepository

---
+++ src/modules/products/typeorm/repositories/IProductsRepository.ts
@@ -0,0 +1,33 @@
+import Product from "../entities/Product";
+
+export type CreateProductDTO = {
+  nome: string;
+  price: number;
+  quantity: number;
+};
+
+export type PaginateParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
+
+export type ProductPaginateProperties = {
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: Product[];
+};
+
+export interface IProductsRepository {
+  create({ nome, price, quantity }: CreateProductDTO): Promise<Product>;
+  save(product: Product): Promise<Product>;
+  findAll({
+    page,
+    skip,
+    take,
+  }: PaginateParams): Promise<ProductPaginateProperties>;
+  findById(id: string): Promise<Product | null>;
+  findByName(name: string): Promise<Product | null>;
+  delete(product: Product): Promise<void>;
+}
